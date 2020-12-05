ffc3cd15b5d019ee5dbd0d78af06227f548b79ae
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-05 18:43:35 -0500

    Create CarInsurance

---
+++ CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Write true or false.");
+            bool hadDUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = age > 15 && !hadDUI && tickets <= 3;
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+
+        }
+    }
+}
