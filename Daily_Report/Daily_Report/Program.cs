1108c279246b225bfcb680b1d2941340e3e15d77
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-04 23:14:08 -0500

    Create Daily Report

---
+++ Daily_Report/Daily_Report/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int page = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string experience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            float hours = float.Parse(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+            //Console.WriteLine(name);
+            //Console.WriteLine(course);
+            //Console.WriteLine(page);
+            //Console.WriteLine(needHelp);
+            //Console.WriteLine(experience);
+            //Console.WriteLine(feedback);
+            //Console.WriteLine(hours);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
