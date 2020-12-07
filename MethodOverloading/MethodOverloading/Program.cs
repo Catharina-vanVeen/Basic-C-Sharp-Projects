46a893da932d23e22b23f3e53b4ab640f328b261
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-06 23:07:19 -0500

    Create MethodOverloading

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MagicMath magic = new MagicMath();
+
+            Console.WriteLine(magic.SurpriseMath(5));
+            Console.WriteLine(magic.SurpriseMath(5.33m));
+            Console.WriteLine(magic.SurpriseMath("5"));
+
+            Console.ReadKey();
+        }
+    }
+}
