55a0bf0c79f622b5d9746850645703c2cc22bdf2
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-06 23:20:36 -0500

    Create MethodWithOptionalParameter

---
+++ MethodWithOptionalParameter/MethodWithOptionalParameter/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalParameter
+{
+    class Math
+    {
+        public int SurpriseMath(int input1, int input2 = 5)
+        {
+            return input1 * input2;
+        }
+    }
+}
---
+++ MethodWithOptionalParameter/MethodWithOptionalParameter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithOptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please provide an integer.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please provide another integer or press enter, if you don't want to provide a second integer.");
+            try
+            {
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(math.SurpriseMath(input1, input2));
+            }
+            catch
+            {
+                Console.WriteLine(math.SurpriseMath(input1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
