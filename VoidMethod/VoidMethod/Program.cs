98bad68923d3fd7d4f53dfaba9ffa154926b1a38
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-06 23:28:41 -0500

    Create VoidMethod

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MysteryOperation mystery = new MysteryOperation();
+            mystery.Mystery(input1: 2, input2: 4);
+            Console.ReadKey();
+        }
+    }
+}
