386e25eebd3bb17742d265c8d7d0dad745804bc2
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-10 16:05:02 -0500

    Create IO

---
+++ IO/IO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number.");
+            File.WriteAllText(@"C:\Users\Owner\Desktop\log.txt", Console.ReadLine());
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Owner\Desktop\log.txt"));
+            Console.ReadKey();
+        }
+    }
+}
