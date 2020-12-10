bc46b33e0840cf009ae808936fc4b06cd2132439
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-10 18:41:54 -0500

    Create assignment 310

---
+++ Assignment310/Assignment310/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string school = "Dawson Academy";
+            var student1 = new Student("Iestyn");
+            Console.WriteLine("{0} is a {1} at {2}.",student1.Name, student1.Year, school);
+            Console.ReadKey();
+
+        }
+    }
+}
