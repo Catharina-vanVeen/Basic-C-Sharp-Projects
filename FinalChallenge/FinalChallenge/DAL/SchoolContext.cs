186f483d8e09e1e6d2ca22980e2071020b7eb6fa
Catharina-vanVeen <71601163+Catharina-vanVeen@users.noreply.github.com>
2020-12-14 22:22:11 -0500

    Create FinalChallenge

---
+++ FinalChallenge/FinalChallenge/DAL/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+using FinalChallenge.Models;
+
+namespace FinalChallenge.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
